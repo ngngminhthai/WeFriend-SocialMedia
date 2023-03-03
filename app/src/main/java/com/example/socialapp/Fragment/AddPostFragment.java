4fcbc83580736562674210aad22b27910d1dbebf
ntvinh4@cmc.com.vn <neulaanh15@gmail.com>
2023-03-03 07:24:15 +0700

    Post screen

---
+++ app/src/main/java/com/example/socialapp/Fragment/AddPostFragment.java
@@ -0,0 +1,198 @@
+package com.neatroots.samplesocial.Fragment;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.neatroots.samplesocial.Model.Post;
+import com.neatroots.samplesocial.Model.User;
+import com.neatroots.samplesocial.R;
+import com.neatroots.samplesocial.databinding.FragmentAddPostBinding;
+import com.squareup.picasso.Picasso;
+
+import java.util.Date;
+
+
+public class AddPostFragment extends Fragment {
+
+    FragmentAddPostBinding binding;
+    Uri uri;
+    FirebaseAuth auth;
+    FirebaseDatabase database;
+    FirebaseStorage storage;
+    ProgressDialog dialog;
+
+    public AddPostFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        storage = FirebaseStorage.getInstance();
+        dialog = new ProgressDialog(getContext());
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentAddPostBinding.inflate(inflater, container, false);
+
+        dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        dialog.setTitle("Post Uploading");
+        dialog.setMessage("Please Wait...");
+        dialog.setCancelable(false);
+        dialog.setCanceledOnTouchOutside(false);
+
+        database.getReference().child("Users")
+                .child(FirebaseAuth.getInstance().getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.exists()){
+                            User user = snapshot.getValue(User.class);
+                            Picasso.get()
+                                    .load(user.getProfile())
+                                    .placeholder(R.drawable.placeholder)
+                                    .into(binding.profileImage);
+                            binding.name.setText(user.getName());
+                            binding.profession.setText(user.getProfession());
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+        binding.postDescription.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String description = binding.postDescription.getText().toString();
+                if (!description.isEmpty()){
+                    binding.postBtn.setBackgroundDrawable(ContextCompat.getDrawable(getContext(), R.drawable.follow_btn_bg));
+                    binding.postBtn.setTextColor(getContext().getResources().getColor(R.color.white));
+                    binding.postBtn.setEnabled(true);
+                }else {
+                    binding.postBtn.setBackgroundDrawable(ContextCompat.getDrawable(getContext(), R.drawable.follow_active_btn));
+                    binding.postBtn.setTextColor(getContext().getResources().getColor(R.color.gray));
+                    binding.postBtn.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        binding.addImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent, 10);
+            }
+        });
+
+        binding.postBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                hideKeyboard();
+
+                dialog.show();
+                final StorageReference reference = storage.getReference().child("posts")
+                        .child(FirebaseAuth.getInstance().getUid())
+                        .child(new Date().getTime()+"");
+                reference.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        reference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                Post post = new Post();
+                                post.setPostImage(uri.toString());
+                                post.setPostedBy(FirebaseAuth.getInstance().getUid());
+                                post.setPostDescription(binding.postDescription.getText().toString());
+                                post.setPostedAt(new Date().getTime());
+
+                                database.getReference().child("posts")
+                                        .push()
+                                        .setValue(post).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                dialog.dismiss();
+                                                binding.postImage.setVisibility(View.GONE);
+                                                binding.postDescription.setText("");
+                                                Toast.makeText(getContext(), "Posted Successfully", Toast.LENGTH_SHORT).show();
+                                            }
+                                        });
+                            }
+                        });
+                    }
+                });
+            }
+        });
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (data.getData() != null){
+            uri = data.getData();
+            binding.postImage.setImageURI(uri);
+            binding.postImage.setVisibility(View.VISIBLE);
+
+            binding.postBtn.setBackgroundDrawable(ContextCompat.getDrawable(getContext(), R.drawable.follow_btn_bg));
+            binding.postBtn.setTextColor(getContext().getResources().getColor(R.color.white));
+            binding.postBtn.setEnabled(true);
+        }
+    }
+
+    public void hideKeyboard() {
+        // Check if no view has focus:
+        View view = getActivity().getCurrentFocus();
+        if (view != null) {
+            InputMethodManager inputManager = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+            inputManager.hideSoftInputFromWindow(view.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+        }
+    }
+}
