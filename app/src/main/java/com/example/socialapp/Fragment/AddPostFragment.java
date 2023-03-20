f5e188be9c5236d1cbd97921157396bb6bbce774
ntvinh4@cmc.com.vn <neulaanh15@gmail.com>
2023-03-21 02:28:50 +0700

    update

--- app/src/main/java/com/example/socialapp/Fragment/AddPostFragment.java
+++ app/src/main/java/com/example/socialapp/Fragment/AddPostFragment.java
@@ -1,6 +1,7 @@
 package com.example.socialapp.Fragment;
 
 
+import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
@@ -14,12 +15,14 @@ import androidx.fragment.app.Fragment;
 
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -36,6 +39,7 @@ import com.example.socialapp.databinding.FragmentAddPostBinding;
 import com.squareup.picasso.Picasso;
 
 import java.util.Date;
+
 
 
 public class AddPostFragment extends Fragment {
@@ -134,29 +138,48 @@ public class AddPostFragment extends Fragment {
         binding.postBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 hideKeyboard();
+
                 dialog.show();
                 final StorageReference reference = storage.getReference().child("posts")
                         .child(FirebaseAuth.getInstance().getUid())
                         .child(new Date().getTime()+"");
-                if(binding.postDescription.getText()!=null&&!binding.postDescription.getText().toString().isEmpty()){
+                if(uri!=null){
+                    reference.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            reference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    Post post = new Post();
+                                    post.setPostImage(uri.toString());
+                                    post.setPostedBy(FirebaseAuth.getInstance().getUid());
+                                    post.setPostDescription(binding.postDescription.getText().toString());
+                                    post.setPostedAt(new Date().getTime());
+
+                                    database.getReference().child("posts")
+                                            .push()
+                                            .setValue(post).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                @Override
+                                                public void onSuccess(Void aVoid) {
+                                                    dialog.dismiss();
+                                                    binding.postImage.setVisibility(View.GONE);
+                                                    binding.postDescription.setText("");
+                                                    Toast.makeText(getContext(), "Posted Successfully", Toast.LENGTH_SHORT).show();
+                                                }
+                                            });
+                                }
+                            });
+                        }
+                    });
+
+                }
+                else{
                     Post post = new Post();
+                    post.setPostedBy(FirebaseAuth.getInstance().getUid());
                     post.setPostDescription(binding.postDescription.getText().toString());
-                    if(uri!=null){
-                        reference.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                            @Override
-                            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                                reference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                                    @Override
-                                    public void onSuccess(Uri uri) {
-                                        post.setPostImage(uri.toString());
-                                        post.setPostedBy(FirebaseAuth.getInstance().getUid());
-                                        post.setPostedAt(new Date().getTime());
-                                    }
-                                });
-                            }
-                        });
-                    }
+                    post.setPostedAt(new Date().getTime());
                     database.getReference().child("posts")
                             .push()
                             .setValue(post).addOnSuccessListener(new OnSuccessListener<Void>() {
